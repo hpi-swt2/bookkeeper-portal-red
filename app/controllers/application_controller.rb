b2a4d5621f35368f24acd2d94dd21c4acbe485b3
Simon Knott <info@simonknott.de>
2022-11-16 15:03:57 +0100

    add description field to profile edit page
    Co-Authored-By: Philipp Keese <philipp.keese@student.hpi.de>
    Co-Authored-By: Callista Graz <callista.gratz@student.hpi.de>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :current_password, :description])
+  end
 end
