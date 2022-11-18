07e55a2470be50e6d5ed0067da7580ddab60b457
Georg Auge <georg.auge01@gmail.com>
2022-11-18 13:01:15 +0100

    feat(api): crud operations for items
    - Resolves #30
    - missing validation and tests for invalid arguments
    
    Co-authored-by: Matthias Cram <matthias.cram@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_112317) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_103832) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
