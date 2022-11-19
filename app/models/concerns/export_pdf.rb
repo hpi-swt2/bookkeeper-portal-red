e40d1b3464231de57969d6f4f251c663ce249b9b
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2022-11-19 20:34:11 +0100

    linting: fix linting

--- app/models/concerns/export_pdf.rb
+++ app/models/concerns/export_pdf.rb
@@ -1,11 +1,11 @@
 module ExportPdf
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    def to_pdf
-        qr_code = RQRCode::QRCode.new(Rails.application.routes.url_helpers.item_url(self, host: "localhost:3000"))
+  def to_pdf
+    qr_code = RQRCode::QRCode.new(Rails.application.routes.url_helpers.item_url(self, host: "localhost:3000"))
 
-        Prawn::Document.new do
-            render_qr_code(qr_code)
-        end.render
-    end
+    Prawn::Document.new do
+      render_qr_code(qr_code)
+    end.render
+  end
 end
