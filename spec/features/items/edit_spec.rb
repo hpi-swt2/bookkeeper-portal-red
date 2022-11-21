2700120d64d06910546a7190480495a02855fcc6
notacop92 <62717141+notacop92@users.noreply.github.com>
2022-11-21 17:10:42 +0100

    fix: add test for new_item and edit_item

---
+++ spec/features/items/edit_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "new item page", type: :feature do
+    let(:item) do
+        Item.create!(
+            name: "Harry Potter",
+            description: "Author: J.K.Rowling"
+        )
+    end
+    before(:each) do
+        item = :item
+    end
+    it "should render succesfully and show item details" do
+        visit edit_item_path(item)
+        page.find_field("item[name]", with: item.name)
+        page.find_field("item[description]", with: item.description)
+    end
+    it "should accept input, redirect and update name in database" do
+        visit edit_item_path(item)
+        page.fill_in "item[name]", with: "Harry Potter und die Kammer des Schreckens"
+        page.find("input[type='submit']").click
+        expect(page).to have_text("Item was successfully updated.")
+        expect((Item.find_by name: "Harry Potter und die Kammer des Schreckens").description).to eq(item.description) 
+    end
+end
