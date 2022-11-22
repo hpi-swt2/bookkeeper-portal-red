d85fcf7fb91703de34850c6be8885dac97c7bc75
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-11-22 11:50:57 +0100

    fix: locale instead of in url

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :items
+      
+  devise_for :users
+  # Defines the root path route ("/")
+  root "landing_page#index"
+  
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
   # https://github.com/heartcombo/devise/blob/main/README.md
-    devise_for :users
-    resources :items
-    # Defines the root path route ("/")
-    root "landing_page#index"
+    
   end
 end
