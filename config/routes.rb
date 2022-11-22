0a081ac506930adf298b278d964877d8378dccfa
Simon Knott <info@simonknott.de>
2022-11-22 16:30:06 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,15 @@ Rails.application.routes.draw do
 
   resources :items
       
+  # https://github.com/heartcombo/devise/blob/main/README.md
   devise_for :users
   # Defines the root path route ("/")
   root "landing_page#index"
+
+  # Profile routes
+  get "/profiles/me", to: "profiles#show_me"
+  get "/profiles/:id", to: "profiles#show", as: :profile
   
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    # https://github.com/heartcombo/devise/blob/main/README.md
-    devise_for :users
-
-    # Profile routes
-    get "/profiles/me", to: "profiles#show_me"
-    get "/profiles/:id", to: "profiles#show", as: :profile
-
-    # Defines the root path route ("/")
-    root "landing_page#index"
   end
 end
