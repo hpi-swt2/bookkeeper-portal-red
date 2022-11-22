34b4c23900c09d2be5f98592ce40081bb7631556
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-11-22 10:18:18 +0100

    fix: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
   # https://github.com/heartcombo/devise/blob/main/README.md
     devise_for :users
-
+    resources :items
     # Defines the root path route ("/")
     root "landing_page#index"
   end
