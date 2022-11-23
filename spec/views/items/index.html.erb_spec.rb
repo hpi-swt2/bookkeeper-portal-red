81f1f722a3818dae1766a7ec2580bd5105c883f5
Pungitius <>
2022-11-23 16:40:52 +0100

    fix: description is not part of tiled view

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -18,6 +18,5 @@ RSpec.describe "items/index", type: :view do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Description".to_s), count: 2
   end
 end
