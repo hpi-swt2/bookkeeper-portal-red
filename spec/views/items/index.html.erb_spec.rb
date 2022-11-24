829461c9c8192598d7bb113a8304d6de10538872
Pungitius <>
2022-11-24 14:21:13 +0100

    fix: removed old tests

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -14,10 +14,5 @@ RSpec.describe "items/index", type: :view do
     ])
   end
 
-  it "renders a list of items" do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Description".to_s), count: 2
-  end
+  pending "test rendered html"
 end
