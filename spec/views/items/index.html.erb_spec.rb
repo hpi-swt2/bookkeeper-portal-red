68866c44af77b89c0bf75d51dd0419e69bf9e7c2
Arkinul <45757532+Arkinul@users.noreply.github.com>
2022-11-24 22:54:01 +0100

    test: basic tests for the index view

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -4,15 +4,45 @@ RSpec.describe "items/index", type: :view do
   before(:each) do
     assign(:items, [
       Item.create!(
-        name: "Name",
-        description: "Description"
+        name: "Communist Manifesto",
+        description: "A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red"
       ),
       Item.create!(
-        name: "Name",
-        description: "Description"
+        name: "The Hitchhikers Guide to the Galaxy",
+        description: "A science fiction comedy adventure"
       )
     ])
   end
 
   pending "test generated html"
+
+  #check, if the html renders without errors
+
+  it "renders the html without errors" do
+    render
+  end
+
+  #check, if the html renders the name of the items
+
+  it "renders the name of the items" do
+    render
+    expect(rendered).to match(/Communist Manifesto/)
+    expect(rendered).to match(/The Hitchhikers Guide to the Galaxy/)
+  end
+
+  #check, if the html doesnt render the description of the items
+
+  it "does not render the description of the items" do
+    render
+    expect(rendered).not_to match(/A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red/)
+    expect(rendered).not_to match(/A science fiction comedy adventure/)
+  end
+
+
+
+
+
+
+
+
 end
