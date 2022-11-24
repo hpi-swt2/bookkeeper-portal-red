c273b1c810ad44bd15bafb46294fe93614b27cc9
Pungitius <>
2022-11-24 14:21:13 +0100

    fix: removed old tests

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -14,27 +14,5 @@ RSpec.describe "items/index", type: :view do
            ])
   end
 
-  pending "test generated html"
-
-  # check, if the html renders without errors
-
-  it "renders the html without errors" do
-    expect(render).not_to be_empty
-  end
-
-  # check, if the html renders the name of the items
-
-  it "renders the name of the items" do
-    render
-    expect(rendered).to match(/Communist Manifesto/)
-    expect(rendered).to match(/The Hitchhikers Guide to the Galaxy/)
-  end
-
-  # check, if the html doesnt render the description of the items
-
-  it "does not render the description of the items" do
-    render
-    expect(rendered).not_to match(/A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red/)
-    expect(rendered).not_to match(/A science fiction comedy adventure/)
-  end
+  pending "test rendered html"
 end
