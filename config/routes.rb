f9fa6fcd58cdf2d8071c7cdae95b73536dc27b1f
Malte Stellmacher <malte.stellmacher@student.hpi.de>
2022-11-25 10:54:40 +0100

    feat(groups): Add group scaffold to model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :items
+  resources :groups
       
   devise_for :users
   # Defines the root path route ("/")
