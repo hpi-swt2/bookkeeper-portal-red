03564b0a582605b6ca64b627cbc28fe68577f2cf
Matthis Clausen <matthis.clausen@student.hpi.de>
2022-11-26 18:10:32 +0100

    feat: add source to qr-code link

--- app/models/concerns/export_pdf.rb
+++ app/models/concerns/export_pdf.rb
@@ -2,8 +2,7 @@ module ExportPdf
   extend ActiveSupport::Concern
 
   def to_pdf
-    qr_code = RQRCode::QRCode.new(Rails.application.routes.url_helpers.item_url(self, host: "localhost:3000"))
-
+    qr_code = RQRCode::QRCode.new(File.join(Rails.application.routes.url_helpers.item_url(self, host: "localhost:3000"), "/?source=qrcode"))
     Prawn::Document.new do
       render_qr_code(qr_code)
     end.render
