c5f53b3e5b14ce35995854734b6d6f2fa5e433c4
Tim Riedel <timriedel00@gmail.com>
2022-11-27 13:21:35 +0100

    disallow null values for dates in lending and reservation

---
+++ db/migrate/20221127115524_disallow_null_for_dates.rb
@@ -0,0 +1,9 @@
+class DisallowNullForDates < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null(:lendings, :started_at, false)
+    change_column_null(:lendings, :due_at, false)
+    
+    change_column_null(:reservations, :starts_at, false)
+    change_column_null(:reservations, :ends_at, false)
+  end
+end
