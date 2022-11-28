350e9aac217cb76b8019a679f89f567f6756331c
gruendlich <adrian.grund@student.hpi.de>
2022-11-28 12:11:18 +0100

    fix: Activated ActionMailer displaying warnings in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # For devise
   # https://github.com/heartcombo/devise#getting-started
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
