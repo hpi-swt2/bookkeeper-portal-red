19e49a7f5068ba8b603ee513cc8bb94d1eb5f52e
gruendlich <adrian.grund@student.hpi.de>
2022-11-28 13:21:30 +0100

    fix: TestNotification Tests fixed (again)

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe NotificationMailer, type: :mailer do
     let(:mail) { NotificationMailer.test_notification }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Test notification")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Test Bookkeeper")
+      expect(mail.to).to eq(["bookkeeper.red@gmail.com"])
+      expect(mail.from).to eq(["bookkeeper.red@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Hi, find me in app/views/notification_mailer/test_notification.html.erb")
     end
   end
 
