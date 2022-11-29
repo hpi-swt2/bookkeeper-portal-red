8e56bae266320ae2940721f0d35f136ec6f516a9
Pungitius <>
2022-11-29 13:15:09 +0100

    fix: enforce rubocop style guide

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.describe "items/index", type: :view do
   before do
     assign(:items, [
-      Item.create!(
-        name: "Communist Manifesto",
-        description: "A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red"
-      ),
-      Item.create!(
-        name: "The Hitchhikers Guide to the Galaxy",
-        description: "A science fiction comedy adventure"
-      )
-    ])
+             Item.create!(
+               name: "Communist Manifesto",
+               description: "A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red"
+             ),
+             Item.create!(
+               name: "The Hitchhikers Guide to the Galaxy",
+               description: "A science fiction comedy adventure"
+             )
+           ])
   end
 
   pending "test generated html"
 
-  #check, if the html renders without errors
+  # check, if the html renders without errors
 
   it "renders the html without errors" do
     render
   end
 
-  #check, if the html renders the name of the items
+  # check, if the html renders the name of the items
 
   it "renders the name of the items" do
     render
@@ -30,19 +30,11 @@ RSpec.describe "items/index", type: :view do
     expect(rendered).to match(/The Hitchhikers Guide to the Galaxy/)
   end
 
-  #check, if the html doesnt render the description of the items
+  # check, if the html doesnt render the description of the items
 
   it "does not render the description of the items" do
     render
     expect(rendered).not_to match(/A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red/)
     expect(rendered).not_to match(/A science fiction comedy adventure/)
   end
-
-
-
-
-
-
-
-
 end
