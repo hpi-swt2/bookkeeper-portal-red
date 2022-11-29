da21083c8e2ff2036fe65cf6b7aa5e93c5245f9b
Arkinul <45757532+Arkinul@users.noreply.github.com>
2022-11-29 12:50:37 +0100

    refactor: deleting blank lines

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "items/index", type: :view do
   before do
     assign(:items, [
-
       Item.create!(
         name: "Communist Manifesto",
         description: "A book about communism, brought to you by Karl Marx, Friedrich Engels and Team Red"
@@ -13,7 +12,6 @@ RSpec.describe "items/index", type: :view do
         description: "A science fiction comedy adventure"
       )
     ])
-
   end
 
   pending "test generated html"
