b8c915cadd64e282a3756f92ab92bd90fe3d55fd
gruendlich <adrian.grund@student.hpi.de>
2022-11-29 12:07:57 +0100

    fix: Move gmail config with credentials to production config.
    Dev config now includes dummy config to avoid errors but no mail sending in dev is possible.

--- config/application.rb
+++ config/application.rb
@@ -16,18 +16,6 @@ module Bookkeeper
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
-    # Action Mailer Configuration for Gmail
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      user_name:            ENV.fetch("MAIL_USERNAME"),
-      password:             ENV.fetch("MAIL_PASSWORD"),
-      authentication:       'plain',
-      enable_starttls_auto: true,
-      open_timeout:         5,
-      read_timeout:         5 }
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # https://github.com/heartcombo/devise#getting-started
   #
 
+  # Dummy Config for Dev Mode -> No Mail sending but no errors
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            "",
+    password:             "",
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
