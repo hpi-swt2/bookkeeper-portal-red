b200bb1bd7b9bacc0c8eeaa59adaa014fd434963
MatthiasCr <matthias.cram@gmail.com>
2022-12-06 18:11:43 +0100

    test: test for item attributes

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -19,6 +19,39 @@ FactoryBot.define do
     number_of_pages { 150 }
     publisher { "Burghard" }
     edition { 1 }
+  end
 
+  factory :movie, class: 'Item' do
+    item_type { "movie" }
+    name { "Harry Potter" }
+    description { "Dumbledore and friends" }
+
+    director { "Chris Columbus" }
+    release_date { "2001-02-21" }
+    format { "dvd" }
+    genre { "fantasy" }
+    language { "english" }
+    fsk { 6 }
+  end
+
+  factory :game, class: 'Item' do
+    item_type { "game" }
+    name { "Skat" }
+    description { "Classic card game" }
+
+    author { "A Legend" }
+    illustrator { "A Myth" }
+    publisher { "Brockhaus" }
+    number_of_players { 3 }
+    playing_time { 60 }
+    language { "english" }
+  end
+
+  factory :other, class: 'Item' do
+    item_type { "other" }
+    name { "Mars" }
+    description { "Elon Musk's dream" }
+
+    category { "Planet" }
   end
 end
