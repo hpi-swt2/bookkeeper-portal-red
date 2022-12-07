32fdeb4068ce127b9607cf5a3bc4768a48b36f59
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2022-12-07 21:42:05 +0100

    feat: add new site to scan a qr code

--- app/models/concerns/export_pdf.rb
+++ app/models/concerns/export_pdf.rb
@@ -2,7 +2,10 @@ module ExportPdf
   extend ActiveSupport::Concern
 
   def to_pdf
-    qr_code = RQRCode::QRCode.new(File.join(Rails.application.routes.url_helpers.item_url(self, host: "localhost:3000"), "/?source=qrcode"))
+    qr_code = RQRCode::QRCode.new(File.join(
+                                    Rails.application.routes.url_helpers.item_url(self,
+                                                                                  host: "localhost:3000"), "/?source=qrcode"
+                                  ))
     Prawn::Document.new do
       render_qr_code(qr_code)
     end.render
