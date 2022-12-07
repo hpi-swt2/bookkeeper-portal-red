c18b27127f34c5c53443b3bc0498975b9c161609
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-12-07 14:56:57 +0100

    feat: language icon in nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def language_flag(locale)
+        if locale == :en
+            updated_locale = :us
+          else
+            updated_locale = locale
+          end
+        content_class = "flag-icon flag-icon-#{updated_locale}"
+        content_tag(:span, nil, class: content_class)
+    end
 end
