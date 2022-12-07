8f6e47540c76ca83f2ac00129b6e33d000b7ab22
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2022-12-07 22:00:11 +0100

    feat: make QR Code in pdf document larger and add the item name above

--- app/models/concerns/export_pdf.rb
+++ app/models/concerns/export_pdf.rb
@@ -4,10 +4,13 @@ module ExportPdf
   def to_pdf
     qr_code = RQRCode::QRCode.new(File.join(
                                     Rails.application.routes.url_helpers.item_url(self,
-                                                                                  host: "localhost:3000"), "/?source=qrcode"
+                                                                                  host: "localhost:3000"),
+                                                                                  "/?source=qrcode"
                                   ))
+    item_name = self.name
     Prawn::Document.new do
-      render_qr_code(qr_code)
+      text "Item: #{item_name}", align: :center, size: 50
+      render_qr_code(qr_code, extent: 300, align: :center)
     end.render
   end
 end
