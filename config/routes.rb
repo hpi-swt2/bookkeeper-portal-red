44dbfeb178920be703b7728c51c9c36dfe52cc2b
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-12-07 19:52:44 +0100

    refac: remove unnesessary scope call in routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,4 @@ Rails.application.routes.draw do
   # Profile routes
   get "/profiles/me", to: "profiles#show_me"
   get "/profiles/:id", to: "profiles#show", as: :profile
-
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    # https://github.com/heartcombo/devise/blob/main/README.md
-  end
 end
