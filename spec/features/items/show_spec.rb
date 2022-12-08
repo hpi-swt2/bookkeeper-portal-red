81a8e854a30ede3c2b42a9b0317c87ecc99d1a9c
johannaschlimme <johanna.schlimme@gmx.de>
2022-12-08 16:13:49 +0100

    fix: issues introduced by rebase

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -10,7 +10,7 @@ describe "show item page", type: :feature do
     @user = FactoryBot.create(:user, email: 'example@mail.com')
     group = FactoryBot.create(:group)
     FactoryBot.create(:membership, user: @user, group: group)
-    FactoryBot.create(:permission, group: group, item: @item, permission_type: :can_lend)
+    FactoryBot.create(:permission, group: group, item: @item, permission_type: :can_borrow)
   end
 
   [[1600, 900], [400, 600]].each do |screen_size|
@@ -29,19 +29,19 @@ describe "show item page", type: :feature do
       it "only shows borrow button if src=qrcode is present" do
         sign_in @user
         visit "#{item_path(@item)}?src=qrcode"
-        expect(page).to have_text(:visible, "Ausleihen")
+        expect(page).to have_text(:visible, "Borrow")
       end
 
       it "doesn't show borrow button if src=qrcode is not present" do
         sign_in @user
         visit item_path(@item)
-        expect(page).not_to have_text(:visible, "Ausleihen")
+        expect(page).not_to have_text(:visible, "Borrow")
       end
 
       it "doesn't show borrow button if src=not-qrcode" do
         sign_in @user
         visit "#{item_path(@item)}?src=not-qrcode"
-        expect(page).not_to have_text(:visible, "Ausleihen")
+        expect(page).not_to have_text(:visible, "Borrow")
       end
     end
   end
