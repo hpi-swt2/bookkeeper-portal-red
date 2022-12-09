1fe6be1655455a13e2601f17849fb8d4c5d87484
Yannis Hofmann <yannis.hofmann@student.hpi.uni-potsdam.de>
2022-12-09 12:21:43 +0100

    feat: make borrow_groups selectable on edit and new (not working yet because of pre-select feature missing Co-authored-by: Matvey Sivashinskiy <Matvey.Sivashinskiy@student.hpi.uni-potsdam.de>

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,4 +2,17 @@ class Permission < ApplicationRecord
   belongs_to :item
   belongs_to :group
   enum :permission_type, can_view: 0, can_borrow: 1, can_manage: 2
+  def self.borrowers_of_item(item_id)
+    groups = []
+    i = 0
+    Permission.all.each do |permission|
+      puts(permission)
+      if permission.item_id = :item_id && permission.permission_type = 'can_borrow'
+        groups[i] = permission.group_id
+        puts(permission.group_id)
+        i = i + 1
+      end
+    return groups
+    end
+  end
 end
