263d5c2e66d49f950fe152ea4fea315fa08d70ea
Wegener <wegener.ben@gmail.com>
2022-12-10 20:48:28 +0100

    fix: rubocop formatting issues #15

--- app/components/notification_inbox_component.rb
+++ app/components/notification_inbox_component.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class NotificationInboxComponent < ViewComponent::Base
-
   def initialize(user_id)
     super
     @notifications = Notification.where(display: true, user_id: user_id).order(:sent).last(30)
   end
-
 end
