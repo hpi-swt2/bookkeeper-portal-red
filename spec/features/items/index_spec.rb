4975c567921ccdd276f9f3c15b6abc90392a08be
notacop92 <62717141+notacop92@users.noreply.github.com>
2022-12-12 12:25:31 +0100

    feat: add tests for new item button on index page

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -4,15 +4,16 @@ describe "index item page", type: :feature do
 
   it "shows drop down menu when 'New Item' is clicked" do
     visit(items_path)
-    page.find('#dropdownMenuButton').click
+    page.find_by_id('dropdownMenuButton').click
     expect(page).to have_css('.dropdown-item')
     expect(page).to have_link('Book', href: '/items/new?item_type=book')
     expect(page).to have_link('Movie')
     expect(page).to have_link('Other')
   end
+
   it "opens new page with respected item type when clicked on type" do
     visit(items_path)
-    page.find('#dropdownMenuButton').click
+    page.find_by_id('dropdownMenuButton').click
     page.find('.dropdown-item', text: 'Book').click
     expect(page).to have_text('Number of pages')
   end
