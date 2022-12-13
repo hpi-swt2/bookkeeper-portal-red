3ca503ad20548218f347d4d79f4d705a353b4c99
gruendlich <adrian.grund@student.hpi.de>
2022-12-13 18:49:16 +0100

    refactor: changed accessing of current user to allow tests to complete again

--- app/components/notification_inbox_component.rb
+++ app/components/notification_inbox_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class NotificationInboxComponent < ViewComponent::Base
-  def initialize(user_id)
+  def initialize(user)
     super
+    user_id = user.nil? ? nil : user.id
     @notifications = Notification.where(display: true, user_id: user_id).order(:sent).last(30)
   end
 end
