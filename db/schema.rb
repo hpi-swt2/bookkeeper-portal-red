ff9cc6b80e59f361bc94b22cc5f9b92c0bf01054
Glitterrosie <rosmarie.debski@gmail.com>
2022-12-13 14:11:25 +0100

    fix:tests failing because of inconsistent use of max_borrowing_period and max_borrowing_days

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_151347) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
     t.integer "item_type"
-    t.bigint "isbn"
-    t.bigint "isbn"
+    t.integer "isbn"
     t.string "author"
     t.date "release_date"
     t.string "genre"
