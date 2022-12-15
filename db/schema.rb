cc826d9d5bbf0a296aa5ba1fe767f31179a16852
Tim Riedel <timriedel00@gmail.com>
2022-12-15 10:34:35 +0100

    fix: change column type of isbn to bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_151347) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_093316) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
