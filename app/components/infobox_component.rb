00563fa8a3cd8ae3962145d434ab53283f49c381
Janis Wehen <janiswehen@gmail.com>
2022-12-28 11:19:27 +0100

    fix: run rubocop

--- app/components/infobox_component.rb
+++ app/components/infobox_component.rb
@@ -2,9 +2,9 @@
 
 class InfoboxComponent < ViewComponent::Base
   def initialize(heading:, body:, bootstrap_classes:)
+    super()
     @heading = heading
     @body = body
     @bootstrap_classes = bootstrap_classes
   end
-
 end
