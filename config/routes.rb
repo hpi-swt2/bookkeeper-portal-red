4aefd77019c1ef2f2538da7ada1b52917956ac02
Karl Broschke <karl.broschke@student.hpi.de>
2023-01-02 12:59:59 +0100

    refactor: remove unneeded routes and their views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :groups do
+  resources :groups, except: [:show, :new] do
     post :leave
   end
   resources :items do
