c6e66faba58fbc66232ad4493f03a79ef01ef336
Kiru Spreu <spreukiru@gmail.com>
2023-01-02 14:48:57 +0100

    try to fix dev

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
+# Model of the current group
 class Group < ApplicationRecord
   validates :name, presence: true
   has_many :memberships, dependent: :destroy
@@ -25,13 +26,13 @@ class Group < ApplicationRecord
   # The set_defaults will only work if the object is new -- see https://stackoverflow.com/questions/29575259/default-values-for-models-in-rails
 
   def set_defaults
-    self.verified = true if verified.nil?
+    self.verified = false if verified.nil? # true for testing
   end
 
-  def display_name
-    display = name.html_safe
+  def display_name 
+    display = name.html_safe # HERE
     html_verified_icon = "<i class='bi bi-patch-check-fill text-warning'></i>"
-    display << html_verified_icon.html_safe if verified
+    display << html_verified_icon.html_safe if verified # and HERE
     display
   end
 end
