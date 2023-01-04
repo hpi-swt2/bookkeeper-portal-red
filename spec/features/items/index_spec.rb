5c47f566d03d283c2524285428b15f807878616a
Pungitius <>
2023-01-04 18:22:49 +0100

    fix: enforce rubocop

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -5,7 +5,7 @@ describe "index item page", type: :feature do
   it "shows drop down menu when 'New Item' is clicked" do
     visit(items_path)
     group = page.find_by_id('dropdown-button-group')
-    group.find_by_id('dropdownMenuButton').click
+    group.find_by('dropdownMenuButton').click
     expect(group).to have_css('.dropdown-item')
     expect(group).to have_link('Book', href: '/items/new?item_type=book')
     expect(group).to have_link('Movie')
@@ -15,7 +15,7 @@ describe "index item page", type: :feature do
   it "opens new page with respected item type when clicked on type" do
     visit(items_path)
     group = page.find_by_id('dropdown-button-group')
-    group.find_by_id('dropdownMenuButton').click
+    group.find_by('dropdownMenuButton').click
     group.find('.dropdown-item', text: 'Book').click
     expect(page).to have_text('Number of pages')
   end
