9a650be18f07d01a55d1c77d55f74dde7b24eb55
Pungitius <>
2023-01-04 18:19:51 +0100

    fix: remove ambiguity from tests

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -4,17 +4,19 @@ describe "index item page", type: :feature do
 
   it "shows drop down menu when 'New Item' is clicked" do
     visit(items_path)
-    page.find_by_id('dropdownMenuButton').click
-    expect(page).to have_css('.dropdown-item')
-    expect(page).to have_link('Book', href: '/items/new?item_type=book')
-    expect(page).to have_link('Movie')
-    expect(page).to have_link('Other')
+    group = page.find_by_id('dropdown-button-group')
+    group.find_by_id('dropdownMenuButton').click
+    expect(group).to have_css('.dropdown-item')
+    expect(group).to have_link('Book', href: '/items/new?item_type=book')
+    expect(group).to have_link('Movie')
+    expect(group).to have_link('Other')
   end
 
   it "opens new page with respected item type when clicked on type" do
     visit(items_path)
-    page.find_by_id('dropdownMenuButton').click
-    page.find('.dropdown-item', text: 'Book').click
+    group = page.find_by_id('dropdown-button-group')
+    group.find_by_id('dropdownMenuButton').click
+    group.find('.dropdown-item', text: 'Book').click
     expect(page).to have_text('Number of pages')
   end
 
