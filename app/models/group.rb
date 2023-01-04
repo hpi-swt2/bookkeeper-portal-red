0ea1c705c44c3ace2ac6697d0c2f592849e75aa0
Tobias Görgens <tobias.goergens@student.hpi.uni-potsdam.de>
2023-01-04 13:29:48 +0100

    feat: add tags to groups and show verified icon if group is verified

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 # Model of the current group
 class Group < ApplicationRecord
+  enum tag: [:verified, :user]
   validates :name, presence: true
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
