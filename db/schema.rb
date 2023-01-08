677e18b90d6c4f094b1fc5bef7fc3ddeaefc998f
Georg Auge <georg.auge01@gmail.com>
2023-01-08 14:39:34 +0100

    fix: run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_110905) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_133416) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_110905) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
     t.integer "item_type"
-    t.bigint "isbn"
+    t.string "isbn"
     t.string "author"
     t.date "release_date"
     t.string "genre"
