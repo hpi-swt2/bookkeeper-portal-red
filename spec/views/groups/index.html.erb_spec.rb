68f25b4d4c2d7e3760a71d3669ecfe6f602fa0a5
Malte Stellmacher <malte.stellmacher@student.hpi.de>
2023-01-14 14:57:34 +0100

    test: check personal group not listed in index

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -9,13 +9,22 @@ RSpec.describe "groups/index", type: :view do
     membership.group
   end
 
+  let(:user) do
+    membership.user
+  end
+
   before do
-    assign(:user, membership.user)
-    assign(:groups, [membership.group])
+    assign(:user, user)
+    assign(:groups, user.groups)
   end
 
   it "renders a list of groups" do
     render
     expect(rendered).to have_text(group.name)
+  end
+
+  it "renders all but personal groups" do
+    render
+    expect(rendered).to have_no_text(user.personal_group.name)
   end
 end
