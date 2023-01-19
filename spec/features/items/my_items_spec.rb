e34c166df54dc1f86ecfeee1d48a0c8c840d3f3c
johannaschlimme <johanna.schlimme@gmx.de>
2023-01-19 18:43:24 +0100

    fix: fix failing tests after dev merge

--- spec/features/items/my_items_spec.rb
+++ spec/features/items/my_items_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe "my items", type: :feature do
     Membership.create(user: user1, group: @group1, role: 1)
 
     # item1 is managed by group1 (user1)
-    item1.manager_groups << group1
-    item1.viewer_groups << group1
+    @item1.manager_groups << @group1
+    @item1.viewer_groups << @group1
   end
 
   it "shows my items" do
@@ -31,22 +31,17 @@ RSpec.describe "my items", type: :feature do
     sign_in user1
     visit my_items_path
 
-    # item1 has 'no-access' status badge when user1 has no permissions
-    expect(page).to have_text(:visible, I18n.t("items.status_badge.no_access"))
-
-    # item1 has 'available' status badge after user1 gets permission
-    FactoryBot.create(:permission, group: @group1, item: @item1, permission_type: :can_borrow)
-    visit my_items_path
+    # item1 has 'available' status badge when user has borrow permission
     expect(page).to have_text(:visible, I18n.t("items.status_badge.available"))
 
-    # item1 has 'reserved-by-me' status badge when user1 reserves it
+    # item1 has 'reserved-by-me' status badge when user reserves it
     FactoryBot.create(:reservation, item_id: @item1.id, user_id: user1.id, starts_at: Time.zone.now,
                                     ends_at: 2.days.from_now)
     visit my_items_path
     expect(page).to have_text(:visible, I18n.t("items.status_badge.reserved_by_me"))
     @item1.cancel_reservation_for(user1)
 
-    # item1 has 'borrowed-by-me' status badge when user1 borrows it
+    # item1 has 'borrowed-by-me' status badge when user borrows it
     Lending.create(item_id: @item1.id, user_id: user1.id, started_at: Time.zone.now, completed_at: nil,
                    due_at: 2.days.from_now)
     visit my_items_path
