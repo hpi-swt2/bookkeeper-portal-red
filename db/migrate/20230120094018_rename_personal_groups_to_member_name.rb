cd8855a77a2d253d848583bb62a65225e85e0d4d
Philipp Keese <philipp.keese@student.hpi.de>
2023-01-24 09:41:09 +0100

    refactor: comment what this migration does

--- db/migrate/20230120094018_rename_personal_groups_to_member_name.rb
+++ db/migrate/20230120094018_rename_personal_groups_to_member_name.rb
@@ -1,3 +1,4 @@
+  # Update the names of all personal groups to the full name of their first member
 class RenamePersonalGroupsToMemberName < ActiveRecord::Migration[7.0]
   def change
     Group.where(tag: :personal_group).each do | group |
