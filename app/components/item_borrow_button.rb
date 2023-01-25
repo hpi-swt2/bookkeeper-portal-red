f1c3040b6346863143b698b6c9bbead55794ca01
Matthis Clausen <matthis.clausen@student.hpi.de>
2023-01-25 17:40:20 +0100

    fix: rubocop fixes

--- app/components/item_borrow_button.rb
+++ app/components/item_borrow_button.rb
@@ -1,9 +1,8 @@
 class ItemBorrowButton < ViewComponent::Base
-    def initialize(text:, action:, bootstrap_classes:)
-      super()
-      @text = text
-      @action = action
-      @bootstrap_classes = bootstrap_classes
-    end
+  def initialize(text:, action:, bootstrap_classes:)
+    super()
+    @text = text
+    @action = action
+    @bootstrap_classes = bootstrap_classes
   end
-  
+end
