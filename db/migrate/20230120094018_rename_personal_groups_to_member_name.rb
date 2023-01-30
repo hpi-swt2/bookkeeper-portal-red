e2044510271a83629f7f4b3d61fd043775ce836c
Philipp Keese <philipp.keese@student.hpi.de>
2023-01-30 11:40:50 +0100

    fix: please rubocop

--- db/migrate/20230120094018_rename_personal_groups_to_member_name.rb
+++ db/migrate/20230120094018_rename_personal_groups_to_member_name.rb
@@ -1,7 +1,7 @@
-  # Update the names of all personal groups to the full name of their first member
+# Update the names of all personal groups to the full name of their first member
 class RenamePersonalGroupsToMemberName < ActiveRecord::Migration[7.0]
   def change
-    Group.where(tag: :personal_group).each do | group |
+    Group.where(tag: :personal_group).each do |group|
       group.name = group.memberships.first.user.full_name
       group.save!
     end
