04e2d080da85f3f65d80fe885c60f1aa4e06808c
MatthiasCr <matthias.cram@gmail.com>
2023-02-02 20:41:23 +0100

    fix: merge conflicts

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,23 +2,11 @@ class AnalyticsController < ApplicationController
   def show
     @lendings = case params[:mode]
                 when "other"
-                  Lending.where(item: current_user.items)
+                  Lending.where(item: current_user.items).order('created_at DESC')
                 else
-                  current_user.lendings
+                  current_user.lendings.order('created_at DESC')
                 end
     @lendings = apply_filter(@lendings)
-  end
-
-  def borrowed_by_me
-    @lendings = current_user.lendings
-    @mode = "borrowed_by_me"
-    render :history
-  end
-
-  def borrowed_from_me
-    @lendings = Lending.where(item: current_user.items).order('created_at DESC')
-    @mode = "borrowed_from_me"
-    render :history
   end
 
   private
