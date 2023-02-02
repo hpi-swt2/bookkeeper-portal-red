5172cd2544ecbc1e516e2cb5660f829ae1820ddf
MatthiasCr <matthias.cram@gmail.com>
2023-02-02 20:11:21 +0100

    feat: history table on analytics page

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,12 @@
 class AnalyticsController < ApplicationController
   def show
+    @lendings = case params[:mode]
+                when "other"
+                  Lending.where(item: current_user.items)
+                else
+                  current_user.lendings
+                end
+    @lendings = apply_filter(@lendings)
   end
 
   def borrowed_by_me
@@ -12,5 +19,13 @@ class AnalyticsController < ApplicationController
     @lendings = Lending.where(item: current_user.items).order('created_at DESC')
     @mode = "borrowed_from_me"
     render :history
+  end
+
+  private
+
+  def apply_filter(lendings)
+    return lendings if params[:item].blank?
+
+    lendings.select { |lending| lending.item.name == params[:item] }
   end
 end
