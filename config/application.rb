60984235096f2fc73c9ed3d8188350eb1de218ab
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2023-02-02 22:02:33 +0100

    style: fix linting

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module Bookkeeper
       "book" => "https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       "movie" => "https://images.pexels.com/photos/7991579/pexels-photo-7991579.jpeg?auto=compress&cs=tinysrgb&w=600",
       "game" => "https://images.pexels.com/photos/776654/pexels-photo-776654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      "other" => "https://images.pexels.com/photos/4464817/pexels-photo-4464817.jpeg?auto=compress&cs=tinysrgb&w=600" }
+      "other" => "https://images.pexels.com/photos/4464817/pexels-photo-4464817.jpeg?auto=compress&cs=tinysrgb&w=600"
+    }
 
     # Configuration for the application, engines, and railties goes here.
     #
