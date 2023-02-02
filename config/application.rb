58f3d071608b95c49127ae7fc1a2f3faa8bb75c0
Leo Kohlenberg <leo.kohlenberg@student.hpi.de>
2023-02-02 21:55:59 +0100

    feat: Add item type specific placeholder images

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ module Bookkeeper
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
+    config.x.placeholder_images = {
+      "book" => "https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "movie" => "https://images.pexels.com/photos/7991579/pexels-photo-7991579.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "game" => "https://images.pexels.com/photos/776654/pexels-photo-776654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "other" => "https://images.pexels.com/photos/4464817/pexels-photo-4464817.jpeg?auto=compress&cs=tinysrgb&w=600" }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
