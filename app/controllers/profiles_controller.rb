fda8434d15083ac829605a03c316a56808a2ea89
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2023-02-03 14:34:04 +0100

    Profile avatar: add remove button

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,4 +10,13 @@ class ProfilesController < ApplicationController
   def edit_me
     redirect_to edit_user_registration_path
   end
+
+  def remove_avatar
+    @user = User.find(params[:id])
+    if current_user == @user && @user.avatar
+      @user.avatar.purge
+      return redirect_to profile_url(@user), notice: I18n.t("items.messages.successfully_destroyed_image")
+    end
+    redirect_to profile_url(@item), notice: I18n.t("profile.messages.not_allowed_to_edit")
+  end
 end
